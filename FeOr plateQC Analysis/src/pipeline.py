a1bdc8eae0e6a8bb2f1a817844205d004ce26937
kimberly42787 <56846342+kimberly42787@users.noreply.github.com>
2025-06-18 14:38:35 -0700

    Update pipeline.py

--- FeOr plateQC Analysis/src/pipeline.py
+++ FeOr plateQC Analysis/src/pipeline.py
@@ -23,37 +23,53 @@ logger = logging.getLogger(__name__)
 
 
 def process_plate(raw_file, output_folders):
+    """
+    Process each plate from the raw data file 
 
+    Extracts individual plate run from the raw file, cleans and normalize data, calculate
+    QC metrics, generates QC plots, and save each file to a base folder for the experiment run
+
+    Parameters:
+    - raw_file (Dataframe): Raw input file containing data for each plate run
+    - output_folders (dict): Dictionary of output folder paths for saving CSV files 
+    and plots
+
+    Returns:
+    - results (dict): Returns a summary of the calculated QC metrics for each plate.
+    """
+    
+    # Identify each plate run 
     index_pairs = raw_indices(raw_file)
 
+    # Assign an empty dictionary for the result summary to be stored in 
     results = {}
 
     # For loop to analyze raw plate data using the indices 
     for i, (start, end) in enumerate(index_pairs):
         try:
-            # Extract plate name from the raw file and change plate name 
+            # Extract plate name from the raw file and change plate name
             plate_name = raw_file.iloc[start, 1] if pd.notna(raw_file.iloc[start, 1]) else f"Plate {i+1}"
             safe_plate_name = safe_name(plate_name)
 
             logger.info(f"Processing Plate {i+1} of {len(index_pairs)}: {safe_plate_name}")
 
-            # Split each plate to be analyzed
+            # Split each plate run from the raw file to be analyzed
             raw_plate = raw_file.iloc[start:end].reset_index(drop=True)
             results[safe_plate_name] = raw_plate
 
-            # Save each plate to the plateRun folder
+            # Save each raw plate data
             plate_run_path = os.path.join(output_folders["plateRun"], f"{safe_plate_name}.csv")
             raw_plate.to_csv(plate_run_path, index=False)
 
             logger.info(f"Saved raw plate data for {safe_plate_name} to {plate_run_path}")
 
-            # Clean and process data 
+            # Clean and process numeric data 
             clean_data = clean_plata_data(raw_plate)
             feor, hoechst = split_feor_hoechst(clean_data)
             norm_data = normalized_data(feor, hoechst)
             p_control, n_control = controls(norm_data)
 
-            # Calculate QC metrics (Signal to Background and Z') and store data 
+            # Calculate QC metrics (Signal to Background and Z-prime)
             s_b, z_prime = calc_metric(p_control, n_control)
             results[safe_plate_name] = {
                 "Signal to Background:": s_b,
@@ -62,14 +78,14 @@ def process_plate(raw_file, output_folders):
 
             logger.info(f"Calculated QC metrics for {safe_plate_name} -- S/B: {s_b:.3f}, Z: {z_prime:.3f}")
 
-            # Save control data 
+            # Save control data to a CSV file
             control_df = create_control_df(p_control, n_control, plate_name)
             control_path = os.path.join(output_folders["controlsData"], f"{safe_plate_name}_controlData.csv")
             control_df.to_csv(control_path, index=False)
 
             logger.info(f"Control data for {safe_plate_name} is saved")
 
-            # Plot data and save
+            # Generate and save plot 
             control_plot = plot_data(control_df, safe_plate_name, s_b, z_prime)
             plot_path = os.path.join(output_folders["qcPlots"], f"{safe_plate_name}_plot.png")
             control_plot.savefig(plot_path)
