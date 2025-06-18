532c4d82e77b1aa0c3881c66f6c99fb8ca1fee1e
kimberly42787 <56846342+kimberly42787@users.noreply.github.com>
2025-06-18 14:29:06 -0700

    Update qc_plotting.py

--- FeOr plateQC Analysis/src/qc_plotting.py
+++ FeOr plateQC Analysis/src/qc_plotting.py
@@ -3,11 +3,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Create a plot for the controls
 def plot_data(df, safe_plate_name, s_b, z_prime):
+    """
+    Generate a QC plot (stripplot) of the control data with annotated QC metrics
+
+    Parameters:
+    - df (Dataframe): Dataframe containing the control data 
+    - safe_plate_name (str): Filename-friendly plate name used for the title and filename
+    - s_b (float): QC metric - Signal to bakcground ratio of the plate
+    - z_prime (float): QC metric - Z-primer score indicating the assay quality
+
+    Returns:
+    - fig: Stripplot of the control data with annotated QC metrics 
+    """
+
     fig = plt.figure(figsize=(4, 5.5))
 
-    # Create strip plot
+    # Create a stripplot with the individual control values
     sns.stripplot(
         data = df,
         x = "Control_Type",
@@ -18,7 +30,7 @@ def plot_data(df, safe_plate_name, s_b, z_prime):
         alpha = 0.6
     )
 
-    # Add a median line
+    # Add a dashed median lines for the positive and negative control
     for idx, control_type in enumerate(["1mM FAC", "Vehicle"]):
         median_val = df[df["Control_Type"] == control_type]["Value"].median()
         plt.hlines (
@@ -30,17 +42,18 @@ def plot_data(df, safe_plate_name, s_b, z_prime):
             linewidth = 2
         )
     
-    # Add labels
-    plt.title(f"Plate QC - {safe_plate_name}")
+    # Set axis labels and plot title
     plt.ylim(0, 5)
     plt.ylabel("FL Ratio, Normalized to Vehicle")
     plt.margins(x = 0.25)
     plt.yticks(np.arange(0, 5, 1))
+    plt.title(f"Plate QC - {safe_plate_name}")
+
 
     # Annotation text to show QC metrics
     annotation_text = f"S/B: {s_b:.3f}\nZ': {z_prime:.3f}"
 
-    # Create the annotation box
+    # Create the annotation box with the annotation_text (QC metrics)
     plt.gca().text(
         0.95, 0.95, 
         annotation_text, 
